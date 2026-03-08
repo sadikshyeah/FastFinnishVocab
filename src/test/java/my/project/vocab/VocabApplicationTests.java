0cf315f42984aa36cf415254242e1cd95d624f40
Sadikshya <sadikshyeah@gmail.com>
2026-03-08 21:26:41 +0200

    Initial project setup

---
+++ src/test/java/my/project/vocab/VocabApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.vocab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
