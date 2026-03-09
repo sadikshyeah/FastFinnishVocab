22f54cdb5e896d6013cafb99a4e5e3c801da182f
Sadikshya <sadikshyeah@gmail.com>
2026-03-09 22:35:33 +0200

    Added User Entity

---
+++ src/main/java/my/project/vocab/domain/User.java
@@ -0,0 +1,80 @@
+package my.project.vocab.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+
+    private String level;
+
+    public User() {}
+
+    public User(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.level = "A1"; // A1 | A2 | B1 | B2 | C1 | C2, default to A1
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
+    
+
