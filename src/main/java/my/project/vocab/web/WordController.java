c4d0f3b10e5bed2b907fe0e6e632399d6ff0373f
Sadikshya <sadikshyeah@gmail.com>
2026-03-22 21:42:11 +0200

    Implementing CRUD method

--- src/main/java/my/project/vocab/web/WordController.java
+++ src/main/java/my/project/vocab/web/WordController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import my.project.vocab.domain.Word;
 import my.project.vocab.domain.WordRepository;
 
 @Controller
@@ -17,5 +19,31 @@ public class WordController {
         model.addAttribute("words", repository.findAll());
         return "wordList";
     }
+    @RequestMapping("/add")
+    public String addWord(Model model) {
+        model.addAttribute("word", new Word());
+        return "addword";
+    }
+    // CREATE - save
+    @RequestMapping("/save")
+    public String save(Word word) {
+        repository.save(word);
+        return "redirect:/wordlist";
+    }
+
+    // DELETE
+    @RequestMapping("/delete/{id}")
+    public String deleteWord(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/wordlist";
+    }
+
+    // UPDATE - show edit form
+    @RequestMapping("/edit/{id}")
+    public String editWord(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("word", repository.findById(id).orElse(null));
+        return "editword";
+    }
+
   
 }
