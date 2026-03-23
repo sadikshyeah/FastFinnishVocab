c4c624552bc7610c8e1a1a1e783c9ab11131bab7
Sadikshya <sadikshyeah@gmail.com>
2026-03-23 12:27:54 +0200

    import request method

--- src/main/java/my/project/vocab/web/WordController.java
+++ src/main/java/my/project/vocab/web/WordController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import my.project.vocab.domain.Word;
 import my.project.vocab.domain.WordRepository;
@@ -25,7 +26,7 @@ public class WordController {
         return "addword";
     }
     // CREATE - save
-    @RequestMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Word word) {
         repository.save(word);
         return "redirect:/wordlist";
