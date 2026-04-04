c42ccface3e54dba052413a836f43ebc9f6a716d
Sadikshya <sadikshyeah@gmail.com>
2026-04-04 21:02:39 +0300

    add method to fetch a random word by level ID

--- src/main/java/my/project/vocab/domain/WordRepository.java
+++ src/main/java/my/project/vocab/domain/WordRepository.java
@@ -1,10 +1,23 @@
 package my.project.vocab.domain;
-import org.springframework.data.repository.CrudRepository;
+
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface WordRepository extends CrudRepository<Word, Long> {
     List<Word> findByLevelName(String name);
 
     List<Word> findByWordFi(String wordFi);
+
+    @Query(value = """
+            SELECT * FROM words
+            WHERE levels_id = :levelId
+            ORDER BY RANDOM()
+            LIMIT 1
+            """, nativeQuery = true)
+    Optional<Word> findRandomByLevelId(@Param("levelId") Long levelId);
 
 }
